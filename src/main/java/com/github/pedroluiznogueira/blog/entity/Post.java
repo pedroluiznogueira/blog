77bf7bd0a4d28d576294a84785901cf8e520bd6a
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-26 01:52:18 -0300

    specifying a column name with @Column

--- src/main/java/com/github/pedroluiznogueira/blog/entity/Post.java
+++ src/main/java/com/github/pedroluiznogueira/blog/entity/Post.java
@@ -4,12 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "content")
     private String content;
 }
