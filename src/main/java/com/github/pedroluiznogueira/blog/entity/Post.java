070bebd094edb2c72ccf317c4f5759563b28110c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-26 01:45:35 -0300

    lombok @AllArgsConstructor to generate a constructor at run time for all attributes

--- src/main/java/com/github/pedroluiznogueira/blog/entity/Post.java
+++ src/main/java/com/github/pedroluiznogueira/blog/entity/Post.java
@@ -1,8 +1,10 @@
 package com.github.pedroluiznogueira.blog.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Post {
     private Long id;
     private String title;
