3665ed7c1dab9fb54c3df0f0ab0ccd8241cf9a8b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-26 01:46:09 -0300

    lombok @NoArgsConstructor to generate the jpa constrcutor

--- src/main/java/com/github/pedroluiznogueira/blog/entity/Post.java
+++ src/main/java/com/github/pedroluiznogueira/blog/entity/Post.java
@@ -2,9 +2,11 @@ package com.github.pedroluiznogueira.blog.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Post {
     private Long id;
     private String title;
