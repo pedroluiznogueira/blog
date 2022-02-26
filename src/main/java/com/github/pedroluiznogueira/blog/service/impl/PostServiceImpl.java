011739a658e24cb74bef31311da9b1380cd1a085
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-26 02:23:04 -0300

    constructor based dependency injection

--- src/main/java/com/github/pedroluiznogueira/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/github/pedroluiznogueira/blog/service/impl/PostServiceImpl.java
@@ -1,11 +1,20 @@
 package com.github.pedroluiznogueira.blog.service.impl;
 
 import com.github.pedroluiznogueira.blog.dto.PostDto;
+import com.github.pedroluiznogueira.blog.repository.PostRepository;
 import com.github.pedroluiznogueira.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     @Override
     public PostDto createPost(PostDto postDto) {
