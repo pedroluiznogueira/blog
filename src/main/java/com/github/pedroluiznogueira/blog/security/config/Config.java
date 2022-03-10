7a94eb52bc8db04d8e77b4f28436735055d2de61
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-03-10 16:51:57 -0300

    configuring end-points security

--- src/main/java/com/github/pedroluiznogueira/blog/security/config/Config.java
+++ src/main/java/com/github/pedroluiznogueira/blog/security/config/Config.java
@@ -1,6 +1,7 @@
 package com.github.pedroluiznogueira.blog.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class Config extends WebSecurityConfigurerAdapter {
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests() // refeering the authorization
+                .anyRequest() // refeering to any request
+                .authenticated() // refeering that they will have to be authenticated
+                .and()
+                .httpBasic(); // with basic auth
+    }
 }
